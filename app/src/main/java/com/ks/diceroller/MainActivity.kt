e3b5912f24e4e72f7404ab47ce29bab609574ea7
Kishan Kr Sharma <skishan781@gmail.com>
2020-05-18 17:27:46 +0530

    Menu Added

--- app/src/main/java/com/ks/diceroller/MainActivity.kt
+++ app/src/main/java/com/ks/diceroller/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.ks.diceroller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -13,12 +16,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var diceImage: ImageView
     //or var diceImage: ImageView ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{
             Toast.makeText(this,"button Clicked",Toast.LENGTH_SHORT).show()
+
             rollDice()
         }
 
@@ -26,8 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
-        val drawableResource = when (randomInt) {
+        val drawableResource = when (Random().nextInt(6) + 1) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -36,6 +40,22 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
         dice_image.setImageResource(drawableResource)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val i = item.itemId
+        when(i){
+            R.id.m1 -> Toast.makeText(this,item.title,Toast.LENGTH_SHORT).show()
+            R.id.m2 -> {
+                startActivity(Intent(this@MainActivity,PdfActivity::class.java))
+            }
+            else -> return false
+        }
+        return true
     }
 }
